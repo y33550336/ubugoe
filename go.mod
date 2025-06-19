3b73a723133d1423778c81e81f431f395cb89456
y33550336 <u5yuugoo@gmail.com>
2025-06-19 18:49:23 +0900

    complete

---
+++ go.mod
@@ -0,0 +1,21 @@
+module ubugoe
+
+go 1.24.4
+
+require (
+	github.com/labstack/echo/v4 v4.13.4
+	github.com/traPtitech/go-traq v0.0.0-20250411085910-749ba86cfa5b
+)
+
+require (
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/oauth2 v0.20.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	traq "github.com/traPtitech/go-traq"
+)
+
+
+func main() {
+	token := os.Getenv("TRAQ_TOKEN")
+
+	client := traq.NewAPIClient(traq.NewConfiguration())
+	auth := context.WithValue(context.Background(), traq.ContextAccessToken, token)
+
+	e := echo.New()
+
+	e.GET("/ubugoe/:userId", func(c echo.Context) error {
+		userID := c.Param("userId")
+		//fmt.Println(channelID)
+
+		channelList, _, _ := client.ChannelApi.GetChannels(auth).Path("gps/times/" + userID).
+			Execute()
+		if len(channelList.Public) == 0{
+			return c.String(404, "No channel")
+		}
+
+		channel := channelList.Public[0]
+		channelID := channel.Id
+
+		messages, _, _ := client.ChannelApi.GetMessages(auth, channelID).
+			Since(time.Date(1970, 1, 1, 0, 0, 0, 0, time.Local)).
+			Limit(10).
+			Order("asc").
+			Execute()
+
+		messageContents := make([]string, 0, len(messages))
+		for _, message := range messages {
+			messageContents = append(messageContents, message.Content)
+		}
+
+		return c.JSON(200, messageContents)
+	})
+
+	e.Start(":8080")
+
+}
